a3298a7e5c45ca83820cfbe0b21ac6fe6a72d06e
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-20 22:24:42 -0300

    Install mongoose and connect to mongodb cluster

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0-lfmbb.mongodb.net/test?retryWrites=true&w=majority`);
+
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
