b10849874ef449f0ef0e28daa385a7a5ccdd0858
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-21 19:44:34 -0300

    Set '/devs' GET route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
+const DevController = require('./controllers/DevController');
+
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  res.send("Hello World!");
-});
+routes.get('/devs', DevController.store);
 
 module.exports = routes;
