1013190095258b51ec2e89ebfa584ed0e08563cf
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-20 21:36:00 -0300

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server is running on port 3333');
+});
