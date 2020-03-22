530d583fae033d1752b4e5157a7e27abf034c27a
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-22 19:22:10 -0300

    Create dislikes controller 'store' method

--- back-end/src/controllers/DislikesController.js
+++ back-end/src/controllers/DislikesController.js
@@ -2,6 +2,22 @@ const Dev = require('../models/Dev');
 
 module.exports = {
   async store(req, res) {
+    const {devID} = req.params;
+    const {userID} = req.headers;
 
+    const loggedDev = await Dev.findById(userID);
+    const targetDev = await Dev.findById(devID);
+
+    if(!targetDev) {
+      return res.status(400).json({error: 'Target dev not found'});
+    }
+
+    loggedDev.dislikes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    console.log(`User ${loggedDev.user} dislikes ${targetDev.user}`);
+    
+    return res.json(loggedDev);
   },
 };
