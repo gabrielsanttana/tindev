ab706055ccc7845953377fabe284799e4c2e1742
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-22 19:27:33 -0300

    Add dislikes controller POST method on routes file

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const DevController = require('./controllers/DevController');
-
+const DislikesController = require('./controllers/DislikesController');
 const routes = express.Router();
 
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devID/dislikes', DislikesController.store);
 
 module.exports = routes;
