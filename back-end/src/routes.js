63a73a7e95b6f682a338a79e2cc8f71f40c54cea
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-22 19:39:32 -0300

    Add dev controller GET method in routes file

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -4,6 +4,7 @@ const DevController = require('./controllers/DevController');
 const DislikesController = require('./controllers/DislikesController');
 const routes = express.Router();
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devID/dislikes', DislikesController.store);
 
