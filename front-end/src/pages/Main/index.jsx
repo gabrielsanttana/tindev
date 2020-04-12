b0ab40a955700834b34b75f76b27e46846085006
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-04-12 14:26:35 -0300

    Add Main page basic structure and add its route on routes file

---
+++ front-end/src/pages/Main/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Main() {
+  return (
+    <div>
+      Main
+    </div>
+  );
+}
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Login from './pages/Login';
+import Main from './pages/Main';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Login} />
+        <Route exact path='/devs/:id' component={Main} />
       </Switch>
     </BrowserRouter>
   );
