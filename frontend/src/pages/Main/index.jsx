16f83b7ea62b876a2be205ff970ec32ee27d481b
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-25 17:35:03 -0300

    Create main page structure

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
 
 import './styles.css';
 
 export default function Main() {
   return (
-    <div>
-      Main
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="Tindev Logo" />
+      </Link>
+
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li>
+              <img src={user.avatar} alt={user.name}/>
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+
+              <div>
+                <button>
+                  <img src="" alt="deslike"/>
+                </button>
+
+                <button>
+                  <img src="" alt="like"/>
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>Sem devs no momento :(</div>
+      )}
+
+      {matchDev && (
+        <div>
+          <img src="" alt=""/>
+
+          <img src="" alt=""/>
+          <strong></strong>
+          <p></p>
+
+          <button>Fechar</button>
+        </div>
+      )}
     </div>
   );
 }
