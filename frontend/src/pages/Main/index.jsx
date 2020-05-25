c4c315a38e763f63f7ddf170aaa612c189e1a3b0
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-25 18:32:41 -0300

    Add main page styles

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,50 +1,148 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
+import api from '../../services/api';
+
 import logo from '../../assets/logo.svg';
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
+import itsamatch from '../../assets/itsamatch.png';
 
 import './styles.css';
 
 export default function Main() {
+  const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(true);
+
+  async function handleDislike() {
+  }
+
+  async function handleLike() {
+  }
+
   return (
     <div className="main-container">
       <Link to="/">
         <img src={logo} alt="Tindev Logo" />
       </Link>
 
-      {users.length > 0 ? (
         <ul>
-          {users.map(user => (
-            <li>
-              <img src={user.avatar} alt={user.name}/>
+            <li key={2}>
+              <img src="https://avatars3.githubusercontent.com/u/47339825?s=460&u=dbf13c0c1865ffa4e2bc8535c2e32bccc957251d&v=4" alt={'Gabriel'}/>
               <footer>
-                <strong>{user.name}</strong>
-                <p>{user.bio}</p>
+                <strong>Gabriel</strong>
+                <p>Software Developer</p>
               </footer>
 
-              <div>
-                <button>
-                  <img src="" alt="deslike"/>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(1)}>
+                  <img src={dislike} alt="dislike" />
                 </button>
 
-                <button>
-                  <img src="" alt="like"/>
+                <button type="button" onClick={() => handleLike()}>
+                  <img src={like} alt="like" />
                 </button>
               </div>
             </li>
-          ))}
+
+            <li key={2}>
+              <img src="https://avatars3.githubusercontent.com/u/47339825?s=460&u=dbf13c0c1865ffa4e2bc8535c2e32bccc957251d&v=4" alt={'Gabriel'}/>
+              <footer>
+                <strong>Gabriel</strong>
+                <p>Software Developer</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(1)}>
+                  <img src={dislike} alt="dislike" />
+                </button>
+
+                <button type="button" onClick={() => handleLike()}>
+                  <img src={like} alt="like" />
+                </button>
+              </div>
+            </li>
+
+            <li key={2}>
+              <img src="https://avatars3.githubusercontent.com/u/47339825?s=460&u=dbf13c0c1865ffa4e2bc8535c2e32bccc957251d&v=4" alt={'Gabriel'}/>
+              <footer>
+                <strong>Gabriel</strong>
+                <p>Software Developer</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(1)}>
+                  <img src={dislike} alt="dislike" />
+                </button>
+
+                <button type="button" onClick={() => handleLike()}>
+                  <img src={like} alt="like" />
+                </button>
+              </div>
+            </li>
+
+            <li key={2}>
+              <img src="https://avatars3.githubusercontent.com/u/47339825?s=460&u=dbf13c0c1865ffa4e2bc8535c2e32bccc957251d&v=4" alt={'Gabriel'}/>
+              <footer>
+                <strong>Gabriel</strong>
+                <p>Software Developer</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(1)}>
+                  <img src={dislike} alt="dislike" />
+                </button>
+
+                <button type="button" onClick={() => handleLike()}>
+                  <img src={like} alt="like" />
+                </button>
+              </div>
+            </li>
+
+            <li key={2}>
+              <img src="https://avatars3.githubusercontent.com/u/47339825?s=460&u=dbf13c0c1865ffa4e2bc8535c2e32bccc957251d&v=4" alt={'Gabriel'}/>
+              <footer>
+                <strong>Gabriel</strong>
+                <p>Software Developer</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(1)}>
+                  <img src={dislike} alt="dislike" />
+                </button>
+
+                <button type="button" onClick={() => handleLike()}>
+                  <img src={like} alt="like" />
+                </button>
+              </div>
+            </li>
+
+            <li key={2}>
+              <img src="https://avatars3.githubusercontent.com/u/47339825?s=460&u=dbf13c0c1865ffa4e2bc8535c2e32bccc957251d&v=4" alt={'Gabriel'}/>
+              <footer>
+                <strong>Gabriel</strong>
+                <p>Software Developer</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(1)}>
+                  <img src={dislike} alt="dislike" />
+                </button>
+
+                <button type="button" onClick={() => handleLike()}>
+                  <img src={like} alt="like" />
+                </button>
+              </div>
+            </li>
         </ul>
-      ) : (
-        <div>Sem devs no momento :(</div>
-      )}
 
       {matchDev && (
-        <div>
-          <img src="" alt="match"/>
+        <div className="match-container">
+          <img src={itsamatch} alt="match"/>
 
-          <img src="" alt="dev-match"/>
-          <strong></strong>
-          <p></p>
+          <img className="avatar" src="https://avatars3.githubusercontent.com/u/47339825?s=460&u=dbf13c0c1865ffa4e2bc8535c2e32bccc957251d&v=4" alt="dev-match"/>
+          <strong>Gabriel Santana</strong>
+          <p>Software Developer</p>
 
           <button>Fechar</button>
         </div>
