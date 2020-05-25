39a304ea34b1181207ac440e9c8df378e3574514
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-25 19:58:12 -0300

    wip

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const DevController = require('./controllers/DevController');
-const DislikesController = require('./controllers/DislikesController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
+
 const routes = express.Router();
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
-routes.post('/devs/:devID/dislikes', DislikesController.store);
+routes.post('/devs/:devID/likes', LikeController.store);
+routes.post('/devs/:devID/dislikes', DislikeController.store);
 
 module.exports = routes;
