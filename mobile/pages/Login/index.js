972684902d92779670d96eff464bad8cb2257f42
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-07-07 22:15:44 -0300

    Create Login page initial files

---
+++ mobile/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles';
+
+function Login() {
+  return (
+    <div style={styles.container}>
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default Login;
